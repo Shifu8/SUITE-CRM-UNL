4f7906012a55fd00345a6b7d6aa3af6610440587
Brandon <mrshifu879@gmail.com>
2026-08-05 08:45:29 -0500

    build: actualizar archivos compilados de traducciones

--- public/legacy/custom/application/Ext/Language/es_ES.lang.ext.php
+++ public/legacy/custom/application/Ext/Language/es_ES.lang.ext.php
@@ -3,6 +3,20 @@
 
 
 /**
+ * Global application language strings for SuiteCRM Campus UNL
+ */
+$app_strings['LBL_ESTADO_ASPIRANTE'] = 'Estado del Aspirante';
+$app_strings['LBL_MAESTRIA_INTERESADA'] = 'Maestría Interesada';
+$app_strings['LBL_CEDULA'] = 'Cédula / Identificación';
+$app_strings['LBL_CICLO_CONVOCATORIA'] = 'Ciclo / Convocatoria';
+$app_strings['LBL_DEPARTMENT'] = 'Maestría / Departamento';
+$app_strings['LBL_LIST_DEPARTMENT'] = 'Maestría';
+$app_strings['LBL_LIST_STATE'] = 'Estado';
+$app_strings['LBL_LIST_ASSIGNED_USER_NAME'] = 'Asignado a';
+$app_strings['LBL_LIST_PRIMARY_ADDRESS_CITY'] = 'Ciudad';
+
+
+/**
  * Cargar dinámicamente listas desplegables de Asesores, Directores y Usuarios desde la BD.
  */
 if (defined('sugarEntry') && sugarEntry) {
--- public/legacy/custom/modules/Leads/Ext/Language/es_ES.lang.ext.php
+++ public/legacy/custom/modules/Leads/Ext/Language/es_ES.lang.ext.php
@@ -3,6 +3,21 @@
 
 
 /**
+ * Module language strings for Leads (Convert Lead view)
+ */
+$mod_strings['LBL_ESTADO_ASPIRANTE']   = 'Estado del Aspirante';
+$mod_strings['LBL_MAESTRIA_INTERESADA'] = 'Maestría Interesada';
+$mod_strings['LBL_CEDULA']              = 'Cédula / Identificación';
+$mod_strings['LBL_CICLO_CONVOCATORIA']  = 'Ciclo / Convocatoria';
+$mod_strings['LBL_DEPARTMENT']          = 'Maestría / Departamento';
+$mod_strings['LBL_PRIMARY_ADDRESS_CITY']= 'Ciudad';
+$mod_strings['LBL_PHONE_MOBILE']        = 'Móvil';
+$mod_strings['LBL_MOBILE_PHONE']        = 'Móvil';
+$mod_strings['LBL_ASSIGNED_TO_NAME']    = 'Asignado a (Director de Maestría)';
+$mod_strings['LBL_ASSIGNED_USER_NAME']  = 'Asignado a (Director de Maestría)';
+
+
+/**
  * Etiquetas de menú personalizadas para Leads
  */
 $mod_strings['LNK_NEW_LEAD']     = 'Crear Cliente Potencial';
